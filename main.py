e42e68fbe22711878b9e472260ed0238a59eeb2f
Kasjan <kasjan.bojko.gdansk@gmail.com>
2023-11-24 22:26:34 +0100

    background

--- main.py
+++ main.py
@@ -5,5 +5,14 @@ from pygame import mixer
 #inicjalizacja pygame
 pygame.init()
 
+bg = pygame.image.load("background.png")
+display = pygame.display.set_mode((800, 800))
+
+def draw_objects():
+    display.blit(bg, (0, 0))
+
+    pygame.display.update()
+
 while True:
-    pass
+
+    draw_objects()
