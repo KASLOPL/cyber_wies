89b8694fc6d554ec6421b5bd24fd31876bc7340d
Kasjan <kasjan.bojko.gdansk@gmail.com>
2023-11-25 22:36:28 +0100

    basic movment, ball

--- main.py
+++ main.py
@@ -6,13 +6,28 @@ from pygame import mixer
 pygame.init()
 
 bg = pygame.image.load("background.png")
+player = pygame.image.load("ball.png")
 display = pygame.display.set_mode((800, 800))
+player_x = 400
+player_y = 600
+vel_x = 5
 
 def draw_objects():
     display.blit(bg, (0, 0))
+    display.blit(player, (player_x, player_y))
 
     pygame.display.update()
 
-while True:
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_a] and player_x > 0:
+        player_x -= vel_x
+    if keys[pygame.K_d] and player_x < 765:
+        player_x += vel_x
 
     draw_objects()
