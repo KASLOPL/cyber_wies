66bd61869e1ed0641c87d34133465003b07ac14a
Kasjan <kasjan.bojko.gdansk@gmail.com>
2023-11-27 13:40:54 +0100

    PyDosc

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import math
 from pygame.locals import *
 from pygame import mixer
 
-#inicjalizacja pygame
+#* Inicjalizacja PyGame
 pygame.init()
 
+#* Zmienne globalne
 bg = pygame.image.load("background_1.png")
 player = pygame.image.load("ball.png")
 display = pygame.display.set_mode((800, 800))
@@ -17,15 +18,34 @@ moving = False
 friction = 0.98
 
 def draw_objects():
+    '''
+        #? Funckaj nie przyjmuje żadnych argumentów oraz nic nie zwraca
+        #? Zadaniem tej funkcji jestwyświetlanie wszystkich potrzebnych obiektów na ekran
+    '''
     display.blit(bg, (0, 0))
     display.blit(player, (player_x, player_y))
 
     pygame.display.update()
 
 def get_distance(mouse_start, mouse_end):
+    '''
+        #? Funckaj przyjmuje jako argumenty dwa tuple 
+        #! mouse_start -> koordynaty myszki w momencie kliknięcia myszki
+        #! mouse_end -> koordynaty myszki w momęcie puszczenia myszki
+        #? Funkcja zwraca dystans pomiędzy tymi dwoma punktami bez zaokrąglania
+    '''
     return math.sqrt((mouse_end[0] - mouse_start[0]) ** 2 + (mouse_end[1] - mouse_start[1]) ** 2)
 
 def get_angle(mouse_start, mouse_end):
+    '''
+        #? Funckaj przyjmuje jako argumenty dwa tuple 
+        #! mouse_start -> koordynaty myszki w momencie kliknięcia myszki
+        #! mouse_end -> koordynaty myszki w momęcie puszczenia myszki
+        #? Funkcja zwraca tupla zawierającego kąt w radnianach oraz sekcje w której znajduje się myszka
+        #? sekcje oznaczone są literami:
+        #! "L" i "R" oznaczające left i right
+        #! "U" i "D" oznaczające up i down
+    '''
     side_a = int(math.fabs(mouse_end[0] - mouse_start[0]))
     side_b = int(math.fabs(mouse_end[1] - mouse_start[1]))
     if mouse_end[0]<mouse_start[0]:
@@ -39,6 +59,12 @@ def get_angle(mouse_start, mouse_end):
     return math.atan2(side_b, side_a), section
 
 def get_direction(mouse_start, mouse_end):
+    '''
+        #? Funckaj przyjmuje jako argumenty dwa tuple 
+        #! mouse_start -> koordynaty myszki w momencie kliknięcia myszki
+        #! mouse_end -> koordynaty myszki w momęcie puszczenia myszki
+        #? Funkcja zwraca tupla zawierającego ilość pikseli do pokonania w osi x oraz osi y dla obliczonego kąta
+    '''
     angle = get_angle(mouse_start, mouse_end)
     if angle[1] == "LU":
         return math.cos(angle[0]), math.sin(angle[0])
@@ -51,12 +77,15 @@ def get_direction(mouse_start, mouse_end):
 
 run = True
 while run:
+    '''
+        #? Główna pętla gry
+        #? można ją zakończyć zmieniając zmienną run = False
+    '''
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_start = pygame.mouse.get_pos()
-            print(mouse_start)
             mouse_down = True
         if event.type == pygame.MOUSEBUTTONUP and mouse_down and not moving:
             mouse_end = pygame.mouse.get_pos()
@@ -68,13 +97,16 @@ while run:
             moving = True
             mouse_down = False
     
+    #* Poruszanie się piłeczki 
+    #* oraz prosta symulacja tarcia
     if moving:
         player_x += direction_x * power
         player_y += direction_y * power
         power *= friction
         if power<0.1:
             moving = False
-    
+
+    #* Granice pola golfowego 
     if player_x<220:
         direction_x*=-1
     if player_x>595:
@@ -84,11 +116,6 @@ while run:
     if player_y>652:
         direction_y*=-1
 
-
-    keys = pygame.key.get_pressed()
-
-    if keys[pygame.K_w]:
-        direction = 0,0
     draw_objects()
 
 pygame.quit()
