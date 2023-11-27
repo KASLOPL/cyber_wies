8065a3aa2df75a1519762263e312fd501cad1c5a
Kasjan <kasjan.bojko.gdansk@gmail.com>
2023-11-27 20:16:00 +0100

    maps

---
+++ maps.py
@@ -0,0 +1,4 @@
+import pygame
+
+blocks_middle_1 = [[pygame.image.load("block.png"), 400, 300], [pygame.image.load("block.png"), 400, 500], [pygame.image.load("block.png"), 500, 200], [pygame.image.load("block.png"), 220, 100]]
+blocks_cyber_1 = [[pygame.image.load("block.png"), 400, 300], [pygame.image.load("block.png"), 400, 500], [pygame.image.load("block.png"), 500, 200], [pygame.image.load("block.png"), 220, 100]]
